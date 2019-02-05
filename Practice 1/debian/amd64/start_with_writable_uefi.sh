f911226e021350fc0203730c5f68c239d3bfb9c7
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-02-05 10:15:09 +0600

    Update Debian links

--- Practice 1/debian/amd64/start_with_writable_uefi.sh
+++ Practice 1/debian/amd64/start_with_writable_uefi.sh
@@ -8,7 +8,7 @@ qemu-system-x86_64                                         \
     $DISPLAY                                               \
     -drive if=pflash,file=flash0.img,format=raw            \
     -drive if=pflash,file=flash1.img,format=raw            \
-    -drive if=none,file=debian-9.2.0-amd64-hd.qcow2,id=hd0 \
+    -drive if=none,file=debian-9.7.0-amd64-hd.qcow2,id=hd0 \
     -device virtio-blk-device,drive=hd0                    \
     -netdev user,hostfwd=tcp::2222-:22,id=eth0             \
     -device virtio-net-device,netdev=eth0
