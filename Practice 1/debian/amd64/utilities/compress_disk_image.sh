ddb0a6281a9fd83c056226449dabd65950bc8849
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-02-19 09:45:57 +0600

    Update Debian links to 9.8

--- Practice 1/debian/amd64/utilities/compress_disk_image.sh
+++ Practice 1/debian/amd64/utilities/compress_disk_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DISK='debian-9.7.0-amd64-hd.qcow2'
+DISK='debian-9.8.0-amd64-hd.qcow2'
 
 test -e "$DISK" && mv "$DISK" "$DISK".old
 qemu-img convert -O qcow2 -c "$DISK".old "$DISK"
--- Practice 1/debian/amd64/utilities/download_debian.sh
+++ Practice 1/debian/amd64/utilities/download_debian.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DEBIAN='debian-9.7.0-amd64-netinst.iso'
-URL="http://mirrors.kernel.org/debian-cd/9.7.0/amd64/iso-cd/$DEBIAN"
+DEBIAN='debian-9.8.0-amd64-netinst.iso'
+URL="http://mirrors.kernel.org/debian-cd/9.8.0/amd64/iso-cd/$DEBIAN"
 
 curl -C - -o "$DEBIAN" "$URL" || wget --continue -o "$DEBIAN" "$URL"
